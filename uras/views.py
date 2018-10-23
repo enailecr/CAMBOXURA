027642589b681865f167e17d2509cccf97a26d19
simoesjonatas <jonatasimoes.js@gmail.com>
2018-10-23 16:26:45 -0300

    add tabela de uras

--- uras/views.py
+++ uras/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UraForm
+from .models import URA 
+import re
+from django_tables2 import RequestConfig
+from .tables import URATable
+
+
 @login_required
 def add(request):
     form = UraForm()
@@ -9,7 +15,9 @@ def add(request):
 
 @login_required
 def list(request):
-    return render(request, 'uras.html')
+    table = URATable(URA.objects.all())
+    RequestConfig(request, paginate={'per_page': 10}).configure(table)
+    return render(request, 'uras.html', {'table': table})
 
 @login_required
 def ura_novo(request):
