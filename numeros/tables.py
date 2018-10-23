e5665c47d325b86924d1a1089169e8784c8194d6
simoesjonatas <jonatasimoes.js@gmail.com>
2018-10-23 13:15:57 -0300

    adicionando o botao editar e excluir em numeros de entrada

--- numeros/tables.py
+++ numeros/tables.py
@@ -9,16 +9,16 @@ from django.utils.html import escape, format_html
 from django_tables2.utils import AttributeDict, ucfirst
 
 class NumeroTable(tables.Table):
-#     # excluir = tables.TemplateColumn(
-#     #         '<form action="/contatos/contato-remove/{{record.id}}/" method="post">{% csrf_token %}<input type="hidden" name="_method" value="Excluir"><button data-toggle="tooltip" title="Please note that deletion cannot be undone" type="submit" class="btn btn-danger btn-xs">Excluir</button></form>',
-#     #     orderable=False,
-#     #     verbose_name=''
-#     #     )
-#     # editar = tables.TemplateColumn(
-#     #         '<form action="/contatos/contato-edita/{{record.id}}/" method="get">{% csrf_token %}<input type="hidden" name="_method" value="Editar"><button data-toggle="tooltip" title="Please note that deletion cannot be undone" type="submit" class="btn btn-danger btn-xs">Editar</button></form>',
-#     #     orderable=False,
-#     #     verbose_name=''
-#     #     )
+    excluir = tables.TemplateColumn(
+            '<form action="/numeros/numero-remove/{{record.id}}/" method="post">{% csrf_token %}<input type="hidden" name="_method" value="Excluir"><button data-toggle="tooltip" title="Please note that deletion cannot be undone" type="submit" class="btn btn-danger btn-xs">Excluir</button></form>',
+        orderable=False,
+        verbose_name=''
+        )
+    editar = tables.TemplateColumn(
+            '<form action="/numeros/numero-edita/{{record.id}}/" method="get">{% csrf_token %}<input type="hidden" name="_method" value="Editar"><button data-toggle="tooltip" title="Please note that deletion cannot be undone" type="submit" class="btn btn-danger btn-xs">Editar</button></form>',
+        orderable=False,
+        verbose_name=''
+        )
     class Meta:
         model = NumeroEntrada
-        fields = ('numero', 'origem', 'atendido','gravaChamada')
+        fields = ('numero', 'origem', 'atendido','gravaChamada','editar','excluir')
--- numeros/urls.py
+++ numeros/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from django.conf.urls import include, url
-from .views import list, add , numero_novo
+from .views import list, add, numero_novo, numero_edita, numero_remove
 
 urlpatterns = [
     path(r'', list),
     path(r'add/',add),
     url(r'numero-novo/',numero_novo, name='numero_novo'),
+    url(r'numero-edita/(?P<id>\d+)/$', numero_edita, name='numero_edita'),
+    url(r'numero-remove/(?P<id>\d+)/$', numero_remove, name='numero_remove'),
 ]
