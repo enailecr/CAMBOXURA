10fbf8ffe1e83b2511372fc579b5f9a677e40148
simoesjonatas <jonatasimoes.js@gmail.com>
2018-10-25 15:05:22 -0300

    edita e exclui anuncio falta o html

--- anuncios/urls.py
+++ anuncios/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from django.conf.urls import include, url
 
-from .views import list, add,anuncio_novo
+from .views import list, add,anuncio_novo,anuncio_edita,anuncio_remove
 
 urlpatterns = [
     path(r'', list),
     path(r'add/',add),
     url(r'anuncio-novo/',anuncio_novo, name='anuncio_novo'),
+    url(r'anuncio-edita/(?P<id>\d+)/$', anuncio_edita, name='anuncio_edita'),
+    url(r'anuncio-remove/(?P<id>\d+)/$', anuncio_remove, name='anuncio_remove'),
 ]
--- anuncios/views.py
+++ anuncios/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render ,redirect
 from django.contrib.auth.decorators import login_required
-
+from django.http import HttpResponse
 from .models import Anuncio 
 import re
 from django_tables2 import RequestConfig
@@ -36,3 +36,43 @@ def anuncio_novo(request):
     anuncio = Anuncio(descricao=descricao, pula=pula,retornaURA=retornaURA, canalNaoResp=canalNaoResp )
     anuncio.save()
     return redirect ('/anuncios/')
+
+
+@login_required
+def anuncio_edita(request, id):
+    data = {}
+    anuncio = Anuncio.objects.get(id=id)
+    data['anuncio'] = anuncio
+    if request.method == 'POST':
+        anuncio = Anuncio.objects.get(id=id)
+        descricao = request.POST['descricao']
+        # repeticao = request.POST['repeticao']
+        if 'pular' in request.POST: 
+            pula = request.POST['pular']
+        else:
+            pula = False
+        if 'retorna' in request.POST:   
+            retornaURA = request.POST['retorna']
+        else:
+            retornaURA = False
+        if 'resposta' in request.POST:   
+            canalNaoResp = request.POST['resposta']
+        else:
+            canalNaoResp = False
+        #destino = request.POST['nome'] 
+        
+        anuncio.descricao = descricao
+        #anuncio.repeticao = repeticao
+        anuncio.pula = pula
+        anuncio.retornaURA = retornaURA
+        anuncio.canalNaoResp = canalNaoResp
+        anuncio.save()        
+        return redirect('/anuncios/')
+    else:
+        return render(request, 'editaAnuncio.html', data)
+
+@login_required
+def anuncio_remove(request, id):
+    anuncio = Anuncio.objects.get(id=id)
+    anuncio.delete()
+    return redirect('/anuncios/')
