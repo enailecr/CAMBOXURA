5efdcec55ac6fc52e5b7f7b1aa9f43fb359438ce
simoesjonatas <jonatasimoes.js@gmail.com>
2018-10-31 14:29:52 -0300

    add excluir e editar de ura part 1 e add sem o form

--- uras/urls.py
+++ uras/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from django.conf.urls import include, url
-from .views import list, add, ura_novo
+from .views import list, add, ura_novo, ura_remove, ura_edita
 
 urlpatterns = [
     path(r'', list),
     path(r'add/',add),
     url(r'ura-novo/',ura_novo, name='ura_novo'),
+    url(r'ura-edita/(?P<id>\d+)/$', ura_edita, name='ura_edita'),
+    url(r'ura-remove/(?P<id>\d+)/$', ura_remove, name='ura_remove'),
 ]
--- uras/views.py
+++ uras/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import UraForm
+
+from django.http import HttpResponse
 from .models import URA 
 import re
 from django_tables2 import RequestConfig
@@ -9,9 +10,7 @@ from .tables import URATable
 
 @login_required
 def add(request):
-    form = UraForm()
-    data = {'form' : form}
-    return render(request, 'UraNovo.html', data)
+    return render(request, 'UraNovo.html')
 
 @login_required
 def list(request):
@@ -21,7 +20,78 @@ def list(request):
 
 @login_required
 def ura_novo(request):
-    form = UraForm(request.POST or None)
-    if form.is_valid():
-        form.save()
+    nome = request.POST['descricao']
+    descricao = request.POST['descricao']
+    anuncioUra = request.POST['descricao']
+    discarDireto = request.POST['descricao']
+    timeout = request.POST['descricao']
+    tentativasInvalidas = request.POST['descricao']
+    gravRepetInvalid = request.POST['descricao']
+
+    if 'descricao' in request.POST: 
+        anexAnuncInvalid = request.POST['descricao']
+    else:
+        anexAnuncInvalid = False
+
+    if 'descricao' in request.POST: 
+        returnInvalid = request.POST['descricao']
+    else:
+        returnInvalid = False
+
+    gravInvalid =request.POST['descricao']
+    # destinoInvalid = models.OneToOneField(
+    #     Destino,
+    #     on_delete=models.CASCADE,
+    #     parent_link=True,
+    #     related_name= 'destinoInvalidURA'
+    # )
+    retentativasTimeout = request.POST['descricao']
+    gravRetentTimeout = request.POST['descricao']
+    if 'descricao' in request.POST: 
+        anexAnuncTimeout = request.POST['descricao']
+    else:
+        anexAnuncTimeout = False
+
+    if 'descricao' in request.POST: 
+        retornarTimeout = request.POST['descricao']
+    else:
+        retornarTimeout = False
+    
+    gravTimeout = request.POST['descricao']
+    # destinoTimeout = models.OneToOneField(
+    #     Destino,
+    #     on_delete=models.CASCADE,
+    #     parent_link=True,
+    #     related_name='destinoTimeoutURA'
+    # )
+    if 'descricao' in request.POST: 
+        returnURACaixaPostal = request.POST['descricao']
+    else:
+        returnURACaixaPostal = False
+
+    ura = URA(nome=nome,descricao=descricao,anuncioUra=anuncioUra, discarDireto=discarDireto,
+    timeout=timeout,tentativasInvalidas=tentativasInvalidas,gravRepetInvalid=gravRepetInvalid,
+    anexAnuncInvalid=anexAnuncInvalid,returnInvalid=returnInvalid,gravInvalid=gravInvalid,
+    retentativasTimeout=retentativasTimeout,gravRetentTimeout=gravRetentTimeout,
+    anexAnuncTimeout=anexAnuncTimeout,retornarTimeout=retornarTimeout,gravTimeout=gravTimeout,
+    returnURACaixaPostal=returnURACaixaPostal)
+    ura.save() 
     return redirect ('/uras/')
+
+@login_required
+def ura_edita(request, id):
+    data = {}
+    ura = URA.objects.get(id=id)
+    data['ura'] = ura
+    if request.method == 'POST':
+           
+        return redirect('/uras/')
+    else:
+        return render(request, 'editaUra.html', data)
+
+
+@login_required
+def ura_remove(request, id):
+    ura = URA.objects.get(id=id)
+    ura.delete()
+    return redirect('/uras/')
