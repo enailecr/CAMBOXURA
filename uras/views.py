d00141d8e6f05f47a5bb0b441c26768620b2153f
CAM <enaile.crebello@gmail.com>
2018-12-03 15:20:55 -0200

    Edição de URA com ramais

--- uras/views.py
+++ uras/views.py
@@ -293,6 +293,11 @@ def ura_edita(request, id):
     data = {}
     ura = URA.objects.get(id=id)
     data['ura'] = ura
+    opcoes = OpcaoURA.objects.filter(ura=ura)
+    data['opcoes'] = opcoes
+    count= len(opcoes)
+    data['count'] = count
+
     if request.method == 'POST':
         nome = request.POST['nome_ura']
         descricao = request.POST['descricao']
@@ -495,6 +500,66 @@ def ura_edita(request, id):
         else:
             ura.gravTimeout = None
             ura.save()
+
+        for op in opcoes:
+            op.delete()
+
+        tipoDestino = []
+        dest_anuncios = []
+        dest_gravacoes = []
+        dest_numeros = []
+        dest_uras = []
+        dest_filas = []
+        dest_chamadasGrupo = []
+        dest_condicoes = []
+        dest_troncos = []
+        ramal = []
+        retornar = []
+
+        contador = int(request.POST['count'])
+        conta= 0
+        for i in range(contador):
+            ramal_num = 'ramal'+str(i)
+            if ramal_num in request.POST:
+                ramal.append(request.POST['ramal'+str(i)])
+                tipoDestino.append(request.POST['tipo_des'+str(i)])
+                dest_anuncios.append(request.POST['dest_anuncios'+str(i)])
+                dest_gravacoes.append(request.POST['dest_gravacoes'+str(i)])
+                dest_numeros.append(request.POST['dest_numeros'+str(i)])
+                dest_uras.append(request.POST['dest_uras'+str(i)])
+                dest_filas.append(request.POST['dest_filas'+str(i)])
+                dest_chamadasGrupo.append(request.POST['dest_chamadasGrupo'+str(i)])
+                dest_condicoes.append(request.POST['dest_condicoes'+str(i)])
+                dest_troncos.append(request.POST['dest_troncos'+str(i)])
+                retorna = 'retorna' + str(i)
+                if retorna in request.POST:
+                    retornar.append(True)
+                else:
+                    retornar.append(False)
+                conta = conta +1
+
+        cont=0;
+        while cont < conta:
+            if tipoDestino[cont] == '1':
+                destino = dest_anuncios[cont]
+            if tipoDestino[cont] == '2':
+                destino = dest_gravacoes[cont]
+            if tipoDestino[cont] == '3':
+                destino = dest_numeros[cont]
+            if tipoDestino[cont] == '4':
+                destino = dest_uras[cont]
+            if tipoDestino[cont] == '5':
+                destino = dest_filas[cont]
+            if tipoDestino[cont] == '6':
+                destino = dest_chamadasGrupo[cont]
+            if tipoDestino[cont] == '7':
+                destino = dest_condicoes[cont]
+            if tipoDestino[cont] == '8':
+                destino = dest_troncos[cont]
+
+            opcaoURA = OpcaoURA(ura = ura,tipoDestino = tipoDestino[cont],destino = destino, ramal=ramal[cont], retornar=retornar[cont])
+            opcaoURA.save()
+            cont= cont + 1
 
         texto = request.user.username + " editou a URA: " +nome
         log = Log(log= texto)
